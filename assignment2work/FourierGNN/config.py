7b889791f3908fccc93967489bc1e5e9884e44a5
Ibrahim Hussain <ibrahimbeaconarion@gmail.com>
2026-04-02 22:53:08 +0500

    fixng datasets file path problems

--- assignment2work/FourierGNN/config.py
+++ assignment2work/FourierGNN/config.py
@@ -6,6 +6,7 @@ System Arguments Expected:
     This module only provides configuration values and expects NO system arguments.
 """
 import torch
+import os
 import dotenv
 from data.data_loader import (
     Dataset_Dhfm,
@@ -27,31 +28,31 @@ data_information = {
         "type": "1"
     },
     "COVID": {
-        "root_path": f"data/covid.csv",
+        "root_path": f"{os.getenv('datasets_FourierGNN_path')}/covid.csv",
         "type": "1"
     },
     "electricity": {
-        "root_path": f"data/electricity.csv",
+        "root_path": f"{os.getenv('datasets_FourierGNN_path')}/electricity.csv",
         "type": "1"
     },
     "solar": {
-        "root_path": f"data/solar",
+        "root_path": f"{os.getenv('datasets_FourierGNN_path')}/solar",
         "type": "1"
     },
     "metr": {
-        "root_path": f"data/metr.csv",
+        "root_path": f"{os.getenv('datasets_FourierGNN_path')}/metr.csv",
         "type": "1"
     },
     "wiki": {
-        "root_path": f"data/wiki.csv",
+        "root_path": f"{os.getenv('datasets_FourierGNN_path')}/wiki.csv",
         "type": "1"
     },
     "crypto": {
-        "root_path": f"data/crypto/daily_20_2190_marked.csv",
+        "root_path": f"{os.getenv('datasets_out_tickerCollector_path')}/crypto/daily_20_2190_marked.csv",
         "type": "1",
     },
     "fx": {
-        "root_path": f"data/fx/daily_10_4506_marked.csv",
+        "root_path": f"{os.getenv('datasets_out_tickerCollector_path')}/fx/daily_10_5118_marked.csv",
         "type": "1",
     },
 }
